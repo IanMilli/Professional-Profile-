7aa643591b5befb457a4ffe4996fe07d1d3774bf
Ian M <ianmillichamp.2@gmail.com>
2023-02-21 14:42:39 +0000

    added js code to swap from first menu to solo authored projects card

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,16 @@
+//use this section to define global variables required for javaScript functions
+let soloProjectsEl = document.getElementById("soloProjects");
 let passwordGeneratorsEl = document.getElementById("passwordGenerators");
 
+soloProjectsEl.addEventListener("click", function (event) {
+event.preventDefault();
+document.getElementById("firstMenuCard").classList.add("d-none");
+document.getElementById("soloProjectsCard").classList.remove("d-none");
+
+});
+
+
 passwordGeneratorsEl.addEventListener("click", function (event) {
-    //area.innerHTML = "";
     event.preventDefault();
     document.getElementById("passwordGeneratorsCard").classList.remove("d-none")
   });
