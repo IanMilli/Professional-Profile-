a8a4ef2d31c65bdec1b020c9c98b91ab3b75fe55
Ian M <ianmillichamp.2@gmail.com>
2023-02-21 15:51:13 +0000

    added section that reveals the revision card div

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 //use this section to define global variables required for javaScript functions
 let soloProjectsEl = document.getElementById("soloProjects");
 let passwordGeneratorsEl = document.getElementById("passwordGenerators");
+let revisionCardsTestEl = document.getElementById("revisionCardsTest");
 
 soloProjectsEl.addEventListener("click", function (event) {
 event.preventDefault();
@@ -12,5 +13,14 @@ document.getElementById("soloProjectsCard").classList.remove("d-none");
 
 passwordGeneratorsEl.addEventListener("click", function (event) {
     event.preventDefault();
+    document.getElementById("row2Star").classList.add("d-none");
+    document.getElementById("soloProjectsCard").classList.add("d-none");
     document.getElementById("passwordGeneratorsCard").classList.remove("d-none")
   });
+
+revisionCardsTestEl.addEventListener("click", function (event){
+event.preventDefault();
+document.getElementById("row2Star").classList.add("d-none");
+    document.getElementById("soloProjectsCard").classList.add("d-none");
+    document.getElementById("revisionTestCard").classList.remove("d-none")
+});
