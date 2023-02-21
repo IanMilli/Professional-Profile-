7a8ea5daee035448479164be601477579b5af736
Ian M <ianmillichamp.2@gmail.com>
2023-02-21 16:27:26 +0000

    added code that allows user to see the readme generator card

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,8 @@ let soloProjectsEl = document.getElementById("soloProjects");
 let passwordGeneratorsEl = document.getElementById("passwordGenerators");
 let revisionCardsTestEl = document.getElementById("revisionCardsTest");
 let dailyPlannerEl = document.getElementById("dailyPlanner");
-let weatherDashboardEl = document.getElementById("weather"); 
+let weatherDashboardEl = document.getElementById("weather");
+let readMeEl = document.getElementById("readMeGenerator"); 
 
 soloProjectsEl.addEventListener("click", function (event) {
 event.preventDefault();
@@ -40,3 +41,10 @@ dailyPlannerEl.addEventListener("click", function (event){
         document.getElementById("soloProjectsCard").classList.add("d-none");
         document.getElementById("weatherCard").classList.remove("d-none")
     });
+
+    readMeEl.addEventListener("click", function (event){
+      event.preventDefault();
+      document.getElementById("row2Star").classList.add("d-none");
+          document.getElementById("soloProjectsCard").classList.add("d-none");
+          document.getElementById("readMeCard").classList.remove("d-none")
+      });
