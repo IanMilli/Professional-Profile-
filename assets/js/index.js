f365c77df8662d9983a00d86ad4a356414bfa3ac
Ian M <ianmillichamp.2@gmail.com>
2023-02-21 16:47:13 +0000

    added code to make the volunteer section hidden until called for

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,7 @@ let revisionCardsTestEl = document.getElementById("revisionCardsTest");
 let dailyPlannerEl = document.getElementById("dailyPlanner");
 let weatherDashboardEl = document.getElementById("weather");
 let readMeEl = document.getElementById("readMeGenerator"); 
+let voluntaryEl = document.getElementById("voluntaryProjects");
 
 soloProjectsEl.addEventListener("click", function (event) {
 event.preventDefault();
@@ -48,3 +49,11 @@ dailyPlannerEl.addEventListener("click", function (event){
           document.getElementById("soloProjectsCard").classList.add("d-none");
           document.getElementById("readMeCard").classList.remove("d-none")
       });
+
+      voluntaryEl.addEventListener("click", function (event){
+event.preventDefault();
+document.getElementById("row2Star").classList.add("d-none");
+document.getElementById("firstMenuCard").classList.add("d-none");
+document.getElementById("row4Star").classList.remove("d-none");
+document.getElementById("voluntaryCard").classList.remove("d-none");
+      })
