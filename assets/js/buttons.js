5fdf2dcad9695277ee75a9750b5f5dde9e0d4bd0
Ian M <ianmillichamp.2@gmail.com>
2023-02-23 17:27:41 +0000

    added code to tidy up collaborated projects and voluntary projects cards

--- assets/js/buttons.js
+++ assets/js/buttons.js
@@ -1,3 +1,5 @@
+//JS file to allow buttons to dynamically alter html and css to reveal and close different divs
+
 //use this section to define global variables required for javaScript functions
 let soloProjectsEl = document.getElementById("soloProjects");
 let passwordGeneratorsEl = document.getElementById("passwordGenerators");
@@ -16,6 +18,10 @@ let closeRevisionTestCardEl = document.getElementById("passwordRevisionHomeButto
 let goBackOneRevisionTestEl = document.getElementById("goBackRevision");
 let closePlannerCardEl = document.getElementById("plannerHomeButton");
 let goBackPlannerEl = document.getElementById("goBackPlanner");
+let closeWeatherCardEl = document.getElementById("weatherHomeButton");
+let goBackWeatherEl = document.getElementById("goBackWeather");
+let closeReadMeCardEl = document.getElementById("readMeHomeButton");
+let goBackReadMeEl = document.getElementById("goBackReadMe");
 
 //code to close first menu card and open solo projects card
 soloProjectsEl.addEventListener("click", function (event) {
@@ -97,8 +103,6 @@ dailyPlannerEl.addEventListener("click", function (event) {
   document.getElementById("dailyPlannerCard").classList.remove("d-none")
 });
 
-
-
 //code to close password revision test card and return to first menu card
 
 closePlannerCardEl.addEventListener("click", function (event){
@@ -118,32 +122,63 @@ goBackPlannerEl.addEventListener("click", function (event){
   // added code causes page to scroll back to work section instead of forcing user to do it
   window.scrollTo(0, document.getElementById('soloProjectsCard').offsetTop)
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
+//code to close solo projects card and open weather dashboard card
 weatherDashboardEl.addEventListener("click", function (event) {
   event.preventDefault();
-  document.getElementById("row2Star").classList.add("d-none");
+  document.getElementById("row3Star").classList.add("d-none");
   document.getElementById("soloProjectsCard").classList.add("d-none");
   document.getElementById("weatherCard").classList.remove("d-none")
 });
+//code to close weather dashboard card and return to first menu card
 
+closeWeatherCardEl.addEventListener("click", function (event){
+  document.getElementById("firstMenuCard").classList.remove("d-none");
+  document.getElementById("row2Star").classList.remove("d-none");
+  document.getElementById("weatherCard").classList.add("d-none");
+  // added code causes page to scroll back to work section instead of forcing user to do it
+  window.scrollTo(0, document.getElementById('work').offsetTop)
+})
+
+//code to close weather dashboard card and return to solo projects card
+goBackWeatherEl.addEventListener("click", function (event){
+  document.getElementById("soloProjectsCard").classList.remove("d-none");
+  document.getElementById("row2Star").classList.add("d-none");
+  document.getElementById("row3Star").classList.remove("d-none");
+  document.getElementById("weatherCard").classList.add("d-none");
+  // added code causes page to scroll back to work section instead of forcing user to do it
+  window.scrollTo(0, document.getElementById('soloProjectsCard').offsetTop)
+})
+//code to open readme card and close solo projects card
 readMeEl.addEventListener("click", function (event) {
   event.preventDefault();
-  document.getElementById("row2Star").classList.add("d-none");
+  document.getElementById("row3Star").classList.add("d-none");
   document.getElementById("soloProjectsCard").classList.add("d-none");
   document.getElementById("readMeCard").classList.remove("d-none")
 });
+//code to close weather dashboard card and return to first menu card
+
+closeReadMeCardEl.addEventListener("click", function (event){
+  document.getElementById("firstMenuCard").classList.remove("d-none");
+  document.getElementById("row2Star").classList.remove("d-none");
+  document.getElementById("readMeCard").classList.add("d-none");
+  // added code causes page to scroll back to work section instead of forcing user to do it
+  window.scrollTo(0, document.getElementById('work').offsetTop)
+})
+
+//code to close weather dashboard card and return to solo projects card
+goBackReadMeEl.addEventListener("click", function (event){
+  document.getElementById("soloProjectsCard").classList.remove("d-none");
+  document.getElementById("row2Star").classList.add("d-none");
+  document.getElementById("row3Star").classList.remove("d-none");
+  document.getElementById("readMeCard").classList.add("d-none");
+  // added code causes page to scroll back to work section instead of forcing user to do it
+  window.scrollTo(0, document.getElementById('soloProjectsCard').offsetTop)
+})
+
+
+
+
+
 //code to close first menu card and open voluntary projects card
 voluntaryEl.addEventListener("click", function (event) {
   event.preventDefault();
